ae91d705b8c5213f9118065782d1eea6f238554d
HimiCos <himicoswilson@gmail.com>
2024-12-28 20:29:54 +0800

    Feature:  ui (#14)

--- lib/main.dart
+++ lib/main.dart
@@ -66,46 +66,9 @@ class MyApp extends StatelessWidget {
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
       title: 'Music Player',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(
-          seedColor: settings.primaryColor,
-          brightness: Brightness.light,
-        ),
-        useMaterial3: true,
-        navigationBarTheme: NavigationBarThemeData(
-          labelBehavior: settings.showNavigationLabels
-              ? NavigationDestinationLabelBehavior.alwaysShow
-              : NavigationDestinationLabelBehavior.onlyShowSelected,
-          height: settings.navigationBarHeight,
-          surfaceTintColor: Colors.transparent,
-          backgroundColor: Colors.white,
-          elevation: 0,
-          shadowColor: Colors.black.withOpacity(0.1),
-          indicatorColor: settings.primaryColor.withOpacity(0.2),
-          iconTheme: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.selected)) {
-              return IconThemeData(color: settings.primaryColor);
-            }
-            return null;
-          }),
-          labelTextStyle: MaterialStateProperty.resolveWith((states) {
-            if (states.contains(MaterialState.selected)) {
-              return TextStyle(color: settings.primaryColor);
-            }
-            return null;
-          }),
-        ),
-        listTileTheme: ListTileThemeData(
-          contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-          minVerticalPadding: 0,
-          visualDensity: VisualDensity.compact,
-        ),
-        dividerTheme: DividerThemeData(
-          color: Colors.grey[200],
-          thickness: 1,
-          space: 1,
-        ),
-      ),
+      themeMode: settings.themeMode,
+      theme: _buildTheme(settings, Brightness.light),
+      darkTheme: _buildTheme(settings, Brightness.dark),
       home: Consumer<AuthProvider>(
         builder: (context, auth, _) {
           if (auth.isLoading) {
@@ -119,5 +82,94 @@ class MyApp extends StatelessWidget {
         },
       ),
     );
+  }
+
+  ThemeData _buildTheme(SettingsProvider settings, Brightness brightness) {
+    final isDark = brightness == Brightness.dark;
+    final baseColor = isDark ? Colors.grey[900]! : Colors.white;
+    final surfaceColor = isDark ? Colors.grey[850]! : Colors.grey[50]!;
+
+    return ThemeData(
+      colorScheme: ColorScheme.fromSeed(
+        seedColor: settings.primaryColor,
+        brightness: brightness,
+      ),
+      useMaterial3: true,
+      navigationBarTheme: NavigationBarThemeData(
+        labelBehavior: settings.showNavigationLabels
+            ? NavigationDestinationLabelBehavior.alwaysShow
+            : NavigationDestinationLabelBehavior.onlyShowSelected,
+        height: settings.navigationBarHeight,
+        surfaceTintColor: Colors.transparent,
+        backgroundColor: baseColor,
+        elevation: 2,
+        shadowColor: isDark
+            ? Colors.black.withOpacity(0.3)
+            : Colors.black.withOpacity(0.1),
+        indicatorColor: settings.primaryColor.withOpacity(0.2),
+        iconTheme: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.selected)) {
+            return IconThemeData(color: settings.primaryColor);
+          }
+          return null;
+        }),
+        labelTextStyle: MaterialStateProperty.resolveWith((states) {
+          if (states.contains(MaterialState.selected)) {
+            return TextStyle(
+              color: settings.primaryColor,
+              fontSize: 12,
+              height: 1.2,
+            );
+          }
+          return const TextStyle(
+            fontSize: 12,
+            height: 1.2,
+          );
+        }),
+      ),
+      listTileTheme: ListTileThemeData(
+        contentPadding: const EdgeInsets.symmetric(horizontal: 16),
+        minVerticalPadding: 0,
+        visualDensity: VisualDensity.compact,
+        tileColor: surfaceColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(settings.listItemBorderRadius),
+        ),
+      ),
+      dividerTheme: DividerThemeData(
+        color: isDark ? Colors.grey[800] : Colors.grey[200],
+        thickness: 1,
+        space: 1,
+      ),
+      cardTheme: CardTheme(
+        elevation: 2,
+        surfaceTintColor: Colors.transparent,
+        color: baseColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+      ),
+      pageTransitionsTheme: PageTransitionsTheme(
+        builders: {
+          for (final platform in TargetPlatform.values)
+            platform: const NoAnimationPageTransitionsBuilder(),
+        },
+      ),
+    );
+  }
+}
+
+class NoAnimationPageTransitionsBuilder extends PageTransitionsBuilder {
+  const NoAnimationPageTransitionsBuilder();
+
+  @override
+  Widget buildTransitions<T>(
+    PageRoute<T> route,
+    BuildContext context,
+    Animation<double> animation,
+    Animation<double> secondaryAnimation,
+    Widget child,
+  ) {
+    return child;
   }
 }
